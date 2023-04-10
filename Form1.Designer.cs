207d7529f4dacf782a425e8a046846fe1e671a04
José Vinícius de Paula Santos <63665043+JVini0166@users.noreply.github.com>
2023-04-10 19:18:10 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LivrariaCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nomeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.valorBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.insertBookButton = new System.Windows.Forms.Button();
+            this.interfaceTable = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.deleteBook = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.interfaceTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Criar Banco em Arquivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nomeBox
+            // 
+            this.nomeBox.Location = new System.Drawing.Point(92, 45);
+            this.nomeBox.Name = "nomeBox";
+            this.nomeBox.Size = new System.Drawing.Size(100, 20);
+            this.nomeBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // valorBox
+            // 
+            this.valorBox.Location = new System.Drawing.Point(92, 97);
+            this.valorBox.Name = "valorBox";
+            this.valorBox.Size = new System.Drawing.Size(100, 20);
+            this.valorBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor";
+            // 
+            // insertBookButton
+            // 
+            this.insertBookButton.Location = new System.Drawing.Point(29, 141);
+            this.insertBookButton.Name = "insertBookButton";
+            this.insertBookButton.Size = new System.Drawing.Size(75, 40);
+            this.insertBookButton.TabIndex = 6;
+            this.insertBookButton.Text = "Inserir Livro";
+            this.insertBookButton.UseVisualStyleBackColor = true;
+            this.insertBookButton.Click += new System.EventHandler(this.insertBookButton_Click);
+            // 
+            // interfaceTable
+            // 
+            this.interfaceTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.interfaceTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nome,
+            this.Valor});
+            this.interfaceTable.Location = new System.Drawing.Point(225, 45);
+            this.interfaceTable.Name = "interfaceTable";
+            this.interfaceTable.Size = new System.Drawing.Size(343, 307);
+            this.interfaceTable.TabIndex = 7;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Valor
+            // 
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 64);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Atualizar Tabela com o Banco";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // deleteBook
+            // 
+            this.deleteBook.Location = new System.Drawing.Point(117, 141);
+            this.deleteBook.Name = "deleteBook";
+            this.deleteBook.Size = new System.Drawing.Size(75, 40);
+            this.deleteBook.TabIndex = 9;
+            this.deleteBook.Text = "Deletar Livro por nome";
+            this.deleteBook.UseVisualStyleBackColor = true;
+            this.deleteBook.Click += new System.EventHandler(this.deleteBook_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 388);
+            this.Controls.Add(this.deleteBook);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.interfaceTable);
+            this.Controls.Add(this.insertBookButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.valorBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nomeBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.interfaceTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nomeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox valorBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button insertBookButton;
+        private System.Windows.Forms.DataGridView interfaceTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button deleteBook;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LivrariaCRUD
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable dt = new DataTable();
+
+        private static String caminho = @"C:\Users\cj3014657\Documents\BancoCRUD\livraria.sqlite";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static SQLiteConnection DbConnection()
+        {
+            var sqliteConnection = new SQLiteConnection("Data Source=" + caminho + "; Version=3;");
+            sqliteConnection.Open();
+            return sqliteConnection;
+        }
+
+        public static void insertBook(int id, String nome, float valor)
+        {
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Livros(id, Nome, Valor ) values (@id, @nome, @valor)";
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.Parameters.AddWithValue("@Nome", nome);
+                    cmd.Parameters.AddWithValue("@Valor", valor);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteConnection.CreateFile(caminho);
+                MessageBox.Show("Banco em Arquivo criado com sucesso em: " + caminho);
+            }
+            catch
+            {
+                throw;
+            }
+
+            try
+               {
+                    using (var cmd = DbConnection().CreateCommand())
+                    {
+                        cmd.CommandText = "CREATE TABLE Livros(id int, Nome Varchar(50), Valor REAL)";
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+
+        public void GetLivros()
+        { 
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                List<string> ImportedFiles = new List<string>();
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Livros";
+                    SQLiteDataReader read = cmd.ExecuteReader();
+                    interfaceTable.Rows.Clear();
+                    while (read.Read())
+                    {
+                        interfaceTable.Rows.Add(new object[] {
+                        read.GetValue(0),  // U can use column index
+                        read.GetValue(1),  // Or column name like this
+                        read.GetValue(2) });
+                    }
+                   }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao se comunicar com o banco");
+            }
+        }
+
+        private int getNextId()
+        {
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "select MAX(id) from Livros;";
+                    string id = cmd.ExecuteScalar().ToString();
+                    if (id == null)
+                    {
+                        return 1;
+                    } else
+                    {
+                        return int.Parse(id) + 1;
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return 1;
+                MessageBox.Show("Erro ao achar o próximo ID");
+            }
+        }
+
+        private void insertBookButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int nextId = getNextId();
+                insertBook(nextId, nomeBox.Text, float.Parse(valorBox.Text));
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Você inseriu algum dado errado: " + ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetLivros();
+        }
+
+        private void deleteBook_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Livros WHERE Nome = @nome";
+                    cmd.Parameters.AddWithValue("@Id", getNextId());
+                    cmd.Parameters.AddWithValue("@Nome", nomeBox.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
