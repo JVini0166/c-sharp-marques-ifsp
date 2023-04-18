bce55ec5507d8627957e801786c994f871457ca0
José Vinícius de Paula Santos <63665043+JVini0166@users.noreply.github.com>
2023-04-17 22:13:53 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnNovoJogo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridViewRanking = new System.Windows.Forms.DataGridView();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wins = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.exibirRankingButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nomeBox = new System.Windows.Forms.TextBox();
+            this.cpfBox = new System.Windows.Forms.TextBox();
+            this.cadastrarButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRanking)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnNovoJogo
+            // 
+            this.BtnNovoJogo.Location = new System.Drawing.Point(15, 35);
+            this.BtnNovoJogo.Name = "BtnNovoJogo";
+            this.BtnNovoJogo.Size = new System.Drawing.Size(93, 33);
+            this.BtnNovoJogo.TabIndex = 0;
+            this.BtnNovoJogo.Text = "Novo Jogo";
+            this.BtnNovoJogo.UseVisualStyleBackColor = true;
+            this.BtnNovoJogo.Click += new System.EventHandler(this.BtnNovoJogo_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(115, 98);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clique";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(234, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(315, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "em";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(315, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Jogo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(315, 122);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(396, 64);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "novo";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(396, 93);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(396, 122);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(47, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(128, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Jogar com a Máquina";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // dataGridViewRanking
+            // 
+            this.dataGridViewRanking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRanking.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.CPF,
+            this.Wins});
+            this.dataGridViewRanking.Location = new System.Drawing.Point(530, 50);
+            this.dataGridViewRanking.Name = "dataGridViewRanking";
+            this.dataGridViewRanking.Size = new System.Drawing.Size(343, 196);
+            this.dataGridViewRanking.TabIndex = 13;
+            this.dataGridViewRanking.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRanking_CellContentClick);
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // CPF
+            // 
+            this.CPF.HeaderText = "CPF";
+            this.CPF.Name = "CPF";
+            // 
+            // Wins
+            // 
+            this.Wins.HeaderText = "Wins";
+            this.Wins.Name = "Wins";
+            // 
+            // exibirRankingButton
+            // 
+            this.exibirRankingButton.Location = new System.Drawing.Point(253, 193);
+            this.exibirRankingButton.Name = "exibirRankingButton";
+            this.exibirRankingButton.Size = new System.Drawing.Size(75, 50);
+            this.exibirRankingButton.TabIndex = 14;
+            this.exibirRankingButton.Text = "Exibir Ranking";
+            this.exibirRankingButton.UseVisualStyleBackColor = true;
+            this.exibirRankingButton.Click += new System.EventHandler(this.exibirRankingButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Nome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "CPF";
+            // 
+            // nomeBox
+            // 
+            this.nomeBox.Location = new System.Drawing.Point(66, 192);
+            this.nomeBox.Name = "nomeBox";
+            this.nomeBox.Size = new System.Drawing.Size(100, 20);
+            this.nomeBox.TabIndex = 18;
+            // 
+            // cpfBox
+            // 
+            this.cpfBox.Location = new System.Drawing.Point(66, 226);
+            this.cpfBox.Name = "cpfBox";
+            this.cpfBox.Size = new System.Drawing.Size(100, 20);
+            this.cpfBox.TabIndex = 19;
+            // 
+            // cadastrarButton
+            // 
+            this.cadastrarButton.Location = new System.Drawing.Point(172, 193);
+            this.cadastrarButton.Name = "cadastrarButton";
+            this.cadastrarButton.Size = new System.Drawing.Size(75, 50);
+            this.cadastrarButton.TabIndex = 20;
+            this.cadastrarButton.Text = "Cadastrar";
+            this.cadastrarButton.UseVisualStyleBackColor = true;
+            this.cadastrarButton.Click += new System.EventHandler(this.cadastrarButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(861, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Para selecionar um jogador, cadastre um usuário, exiba o ranking e clique no CPF " +
+    "do jogador cadastrado. Quando preencher sozinho os campos Nome e CPF ele estará " +
+    "selecionado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 316);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cadastrarButton);
+            this.Controls.Add(this.cpfBox);
+            this.Controls.Add(this.nomeBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.exibirRankingButton);
+            this.Controls.Add(this.dataGridViewRanking);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnNovoJogo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRanking)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnNovoJogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridViewRanking;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Wins;
+        private System.Windows.Forms.Button exibirRankingButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nomeBox;
+        private System.Windows.Forms.TextBox cpfBox;
+        private System.Windows.Forms.Button cadastrarButton;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        bool jogador1 = true;
+        int jogadas = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Player> players = new List<Player>();
+        private string playersFile = "players.txt";
+        private bool playAgainstComputer = false;
+        private Random random = new Random();
+
+        public class Player
+        {
+            public string Name { get; set; }
+            public string Cpf { get; set; }
+            public int Wins { get; set; }
+        }
+
+        private void SavePlayers()
+        {
+            using (StreamWriter sw = new StreamWriter(playersFile))
+            {
+                foreach (var player in players)
+                {
+                    sw.WriteLine($"{player.Name},{player.Cpf},{player.Wins}");
+                }
+            }
+        }
+
+        private bool ValidateCpf(string cpf)
+        {
+            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (cpf == new string(i.ToString()[0], 11))
+                    return false;
+            }
+
+            int[] firstMultiplier = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] secondMultiplier = new int[] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * firstMultiplier[i];
+
+            int result = sum % 11;
+            result = result < 2 ? 0 : 11 - result;
+
+            tempCpf += result.ToString();
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * secondMultiplier[i];
+
+            result = sum % 11;
+            result = result < 2 ? 0 : 11 - result;
+
+            tempCpf += result.ToString();
+
+            return cpf == tempCpf;
+        }
+
+        private void UpdatePlayerWins(string name)
+        {
+            var player = players.FirstOrDefault(p => p.Name == name);
+
+            if (player != null)
+            {
+                player.Wins++;
+
+                // Encontre a linha correspondente ao jogador vencedor no DataGridView
+                foreach (DataGridViewRow row in dataGridViewRanking.Rows)
+                {
+                    if (row.Cells["Nome"].Value.ToString() == player.Name)
+                    {
+                        // Incremente o valor da coluna Wins para esse jogador
+                        int currentWins = Convert.ToInt32(row.Cells["Wins"].Value);
+                        row.Cells["Wins"].Value = currentWins + 1;
+                        break;
+                    }
+                }
+
+                SavePlayers();
+            }
+        }
+
+        private void LoadPlayers()
+        {
+            if (File.Exists(playersFile))
+            {
+                using (StreamReader sr = new StreamReader(playersFile))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(',');
+                        players.Add(new Player { Name = data[0], Cpf = data[1], Wins = int.Parse(data[2]) });
+                    }
+                }
+            }
+        }
+
+        private void MakeComputerMove()
+        {
+            List<Button> availableButtons = new List<Button>();
+
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is Button && ctrl.Name.StartsWith("button") && ctrl.Text == "")
+                {
+                    availableButtons.Add((Button)ctrl);
+                }
+            }
+
+            if (availableButtons.Count > 0)
+            {
+                int index = random.Next(availableButtons.Count);
+                Button btn = availableButtons[index];
+                btn.Text = "O";
+                btn.ForeColor = Color.Blue;
+
+                jogador1 = !jogador1;
+                jogadas++;
+
+                VerificarVitoria();
+            }
+        }
+
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.Text == "")
+            {
+                if (jogador1)
+                {
+                    btn.Text = "X";
+                    btn.ForeColor = Color.Red;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    btn.ForeColor = Color.Blue;
+                }
+
+                jogador1 = !jogador1;
+                jogadas++;
+
+                VerificarVitoria();
+
+                if (!jogador1 && playAgainstComputer)
+                {
+                    MakeComputerMove();
+                }
+            }
+        }
+
+        private void VerificarVitoria()
+        {
+            bool vitoria = false;
+
+            // Verifica linhas
+            for (int i = 1; i <= 9; i += 3)
+            {
+                if (VerificarCelulas(i, i + 1, i + 2))
+                {
+                    vitoria = true;
+                    break;
+                }
+            }
+
+            // Verifica colunas
+            for (int i = 1; i <= 3; i++)
+            {
+                if (VerificarCelulas(i, i + 3, i + 6))
+                {
+                    vitoria = true;
+                    break;
+                }
+            }
+
+            // Verifica diagonais
+            if (VerificarCelulas(1, 5, 9) || VerificarCelulas(3, 5, 7))
+            {
+                vitoria = true;
+            }
+
+            if (vitoria)
+            {
+                FinalizarJogo("Vitória!");
+                UpdatePlayerWins(nomeBox.Text);
+            }
+            else if (jogadas == 9)
+            {
+                FinalizarJogo("Empate!");
+            }
+        }
+
+        private bool VerificarCelulas(int a, int b, int c)
+        {
+            Button btn1 = (Button)Controls["button" + a.ToString()];
+            Button btn2 = (Button)Controls["button" + b.ToString()];
+            Button btn3 = (Button)Controls["button" + c.ToString()];
+
+            if (btn1.Text != "" && btn1.Text == btn2.Text && btn2.Text == btn3.Text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void FinalizarJogo(string mensagem)
+        {
+            lblStatus.Text = mensagem;
+            lblStatus.Visible = true;
+
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is Button && ctrl.Name.StartsWith("button"))
+                {
+                    ((Button)ctrl).Enabled = false;
+                }
+            }
+        }
+        
+        private void BtnNovoJogo_Click_1(object sender, EventArgs e)
+        {
+            jogador1 = true;
+            jogadas = 0;
+            lblStatus.Visible = false;
+
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is Button && ctrl.Name.StartsWith("button"))
+                {
+                    ((Button)ctrl).Text = "";
+                    ((Button)ctrl).ForeColor = Color.Black;
+                    ((Button)ctrl).Enabled = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            playAgainstComputer = checkBox1.Checked;
+        }
+
+        private void exibirRankingButton_Click(object sender, EventArgs e)
+        {
+            dataGridViewRanking.Rows.Clear();
+            var sortedPlayers = players.OrderByDescending(p => p.Wins).Take(10);
+            foreach (var player in sortedPlayers)
+            {
+                dataGridViewRanking.Rows.Add(player.Name, player.Cpf, player.Wins);
+            }
+        }
+
+        private void cadastrarButton_Click(object sender, EventArgs e)
+        {
+            string nome = nomeBox.Text;
+            string cpf = cpfBox.Text;
+
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(cpf) || !ValidateCpf(cpf))
+            {
+                MessageBox.Show("Por favor, insira um nome e um CPF válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                players.Add(new Player { Name = nome, Cpf = cpf, Wins = 0 });
+                SavePlayers();
+                MessageBox.Show("Jogador cadastrado com sucesso!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                nomeBox.Clear();
+                cpfBox.Clear();
+            }
+        }
+
+        private void dataGridViewRanking_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewRanking.CurrentRow != null)
+            {
+                int selectedRowIndex = dataGridViewRanking.CurrentRow.Index;
+
+                if (selectedRowIndex >= 0)
+                {
+                    // Obter os detalhes do jogador selecionado
+                    string cpf = dataGridViewRanking.Rows[selectedRowIndex].Cells["Cpf"].Value.ToString();
+                    string nome = dataGridViewRanking.Rows[selectedRowIndex].Cells["Nome"].Value.ToString();
+
+                    // Preencha os campos apropriados com os detalhes do jogador (por exemplo, txtCpfJogador1)
+                    cpfBox.Text = cpf;
+                    nomeBox.Text = nome;
+                }
+            }
+        }
+    }
+}
